5cfeb8c57a9f35a50e2fd9ba14accb15d75c7191
SamandarbekYr <SamandarbekYR@gmail.com>
2024-02-11 11:11:41 +0500

    Add project files.

---
+++ SignalRTest/Hubs/SendMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.Hubs
+{
+    public class SendMessage : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync(user, message);
+        }
+    }
+}
---
+++ SignalRTest/Models/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRTest.Models
+{
+    public class UserRegister
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
