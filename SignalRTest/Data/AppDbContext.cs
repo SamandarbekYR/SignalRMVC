403bb5f41a3b9305617479f5634131c665c8aad0
SamandarbekYr <SamandarbekYR@gmail.com>
2024-02-11 11:17:31 +0500

    Model Created

---
+++ SignalRTest/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRTest.Models;
+
+namespace SignalRTest.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource =SignalRWPF.db ");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
